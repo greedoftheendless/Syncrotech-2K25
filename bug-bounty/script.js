150f0e406fc8b640c2eb15ef5dbb04eedaff853f
greedoftheendless <greedoftheendless@gmail.com>
2025-09-25 00:37:08 +0530

    added first two challenges

---
+++ bug-bounty/script.js
@@ -0,0 +1,5 @@
+console.log("The first flag is: CTF{c0ns0l3_fl4g}");
+
+document.getElementById("popup-btn").addEventListener("click", function() {
+    alert("The first part of the flag is: CTF{p4rt_0n3}");
+});

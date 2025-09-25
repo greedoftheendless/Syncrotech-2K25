fe7c59dfdb23407ac5ac83d3f3052f6607af33d7
greedoftheendless <greedoftheendless@gmail.com>
2025-09-25 14:20:16 +0530

    made extra flags

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("The first flag is: CTF{c0ns0l3_fl4g}");
 
-document.getElementById("popup-btn").addEventListener("click", function() {
-    alert("The first part of the flag is: CTF{p4rt_0n3}");
+document.getElementById("popup-btn").addEventListener("click", function () {
+  alert("The first part of the flag is: CTF{p4rt_0n3}");
 });
+
+// A hidden path is stored in a variable, can you find it?
+var hiddenPath = "1a2b3c4d5e6f.html";
